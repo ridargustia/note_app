867a897f70f848a98760fcddbe9f832a18fb7cdc
Ridar Gustia <ridargp@gmail.com>
2022-10-08 22:58:56 +0700

    Integrasi Firebase dengan Project ReactJS (Atomic Design) #2

---
+++ src/config/firebase/index.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+// import 'firebase/auth';
+// import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCbaMWqmS6XymgZetsxXRiF_ipovQosShs",
+    authDomain: "notes-app-firebase-6c46b.firebaseapp.com",
+    projectId: "notes-app-firebase-6c46b",
+    storageBucket: "notes-app-firebase-6c46b.appspot.com",
+    messagingSenderId: "678393048664",
+    appId: "1:678393048664:web:280be7215528c4d96a642d",
+    measurementId: "G-BJEZJBK62E"
+};
+
+// Initialize Firebase
+const appFirebaseConfig = initializeApp(firebaseConfig);
+
+export default appFirebaseConfig;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './containers/pages/App';
 import reportWebVitals from './reportWebVitals';
+import appFirebaseConfig from './config/firebase';
+
+console.log('Config Firebase Test ==> ', appFirebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
